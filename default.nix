2fbf43f4810d0d3e998d6ca9ed7820e0881c9a38
root <root@f0995ad6-e377-4aec-b981-20099a9dd2ac>
2023-01-18 01:29:05 +0100

    parsing rules

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 # -*- compile-command: "nix-shell --run 'cabal exec -- ghc-pkg list'"; -*-
-{ pkgs ? import <nixpkgs> {}, sources ? import ./nix/sources.nix {} }:
+{ pkgs ? import (import ./nix/sources.nix {}).nixpkgs {}, sources ? import ./nix/sources.nix {} }:
   # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/make-package-set.nix
 pkgs.haskellPackages.developPackage {
     root = ./.;
@@ -9,7 +9,11 @@ pkgs.haskellPackages.developPackage {
       yahp = self.callCabal2nix "yahp" sources.yahp {};
       interval = self.callCabal2nix "interval" sources.interval {};
     };
+    source-overrides = {
+      # vector-algorithms = "0.9.0.1";
+      chronos = sources.chronos;
+    };
     modifier = with pkgs.haskell.lib; drv:
       disableLibraryProfiling (dontHaddock (addBuildTools drv
-        (with pkgs.haskellPackages; [ cabal-install ghcid])));
+        (with pkgs.haskellPackages; [cabal-install ghcid])));
   }
